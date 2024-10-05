9bade4aadca2ae22dde8ec08c1885e86850d6f77
Jhony-De-Leon <jdeleonp29@miumg.edu.gt>
2024-10-04 18:32:10 -0600

    MODIFICACION JA

--- EnBlanco.cpp
+++ EnBlanco.cpp
@@ -1,1 +1,12 @@
 //Por favor probar
+#include <iostream>
+using namespace std;
+
+int main(){
+	enum DiasSemana {
+        Primavera = 1,
+        Verano,
+        Otono,
+        Invierno
+    };
+}
