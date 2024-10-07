7480cdc9d2a883571945dd16cf747552d23d0612
Jhony-De-Leon <jdeleonp29@miumg.edu.gt>
2024-10-06 23:05:23 -0600

    PRUEBA PARA GEOFREY

--- EnBlanco.cpp
+++ EnBlanco.cpp
@@ -12,3 +12,24 @@ int main(){
 }
 //Agrego un comentario de lo que vamos a trabajar hoy GEO30
 //prueba 
+int numero;
+
+    cout << "Ingrese un numero del 1 al 4: ";
+    cin >> numero;
+    switch(numero) {
+        case Primavera:
+            cout << "Es Primavera" << endl;
+            break;
+        case Verano:
+            cout << "Es Verano" << endl;
+            break;
+        case Otono:
+            cout << "Es Otono" << endl;
+            break;
+        case Invierno:
+            cout << "Es Invierno" << endl;
+            break;
+        default:
+            cout << "Numero invalido. Por favor ingrese un numero del 1 al 4." << endl;
+            break;
+    }
